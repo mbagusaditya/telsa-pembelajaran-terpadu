bd5ab577f7ff97d6f5dafcccf63d2e0d5400203f
mbagusaditya <bagusaditya9a19.dev@gmail.com>
2026-08-14 16:51:33 +0700

    feat: add shadcn-svelte

--- resources/js/pages/index.svelte
+++ resources/js/pages/index.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    const name = "hello"
+    import Button from "@/components/ui/button/button.svelte";
 </script>
 
-<h1 class="text-2xl">
-{name}
-</h1>
+<Button>Tes button</Button>
