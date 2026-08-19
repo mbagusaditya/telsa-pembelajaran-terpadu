82485f506dbd62f9bb75530b1161355c11b09ca1
mbagusaditya <bagusaditya9a19.dev@gmail.com>
2026-08-19 15:14:44 +0700

    feat: add base for student show page

---
+++ resources/js/components/module/dashboard/admin/infolist/student-infolist.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import * as FormControl from '@/components/core/form-control';
+
+    let { student } = $props();
+
+    let studentData = $derived([
+        {
+            label: 'Nama',
+            value: student.data.name,
+        },
+        {
+            label: 'NIK',
+            value: student.data.nik,
+        },
+        {
+            label: 'NIS',
+            value: student.data.nis,
+        },
+        {
+            label: 'NISN',
+            value: student.data.nisn,
+        },
+        {
+            label: 'Tempat lahir',
+            value: student.data.birth_place,
+        },
+        {
+            label: 'Tanggal lahir',
+            value: student.data.birth_date,
+        },
+        {
+            label: 'Jenis kelamin',
+            value: student.data.gender,
+        },
+        {
+            label: 'Tahun masuk',
+            value: student.data.admission_year,
+        },
+        {
+            label: 'Email',
+            value: student.data.user.email,
+        },
+        {
+            label: 'Dibuat oleh',
+            value: student.data.name,
+        },
+    ]);
+</script>
+
+<!-- {JSON.stringify(student)} -->
+
+<div class="grid grid-cols-1 lg:grid-cols-[auto_1fr] gap-4">
+    <FormControl.PreviewImage
+        initialUrl={student.data.user.avatar
+            ? `/storage/${student.data.user.avatar}`
+            : null}
+        aspectRatio="3:4"
+    />
+
+    <table class="h-fit">
+        <colgroup>
+            <col class="w-32" />
+            <col class="w-2" />
+            <col />
+        </colgroup>
+        <tbody class="*:*:py-2 *:border-b *:border-muted">
+            {#each studentData as data}
+                <tr>
+                    <td>{data.label}</td>
+                    <td>:</td>
+                    <td>
+                        {data.value}
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
---
+++ resources/js/pages/dashboard/admin/students/show.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import DashboardLayout from '@/layouts/dashboard.svelte';
+    import StudentInfolist from '@/components/module/dashboard/admin/infolist/student-infolist.svelte';
+    import { type BreadcrumbItem } from '@/types/navigation';
+    import { inertia } from '@inertiajs/svelte';
+    import { route } from '@/generated/helpers/route';
+    import { ArrowLeftIcon } from '@lucide/svelte';
+
+    let { title, student } = $props();
+
+    const breadcrumbItems: BreadcrumbItem[] = [
+        {
+            label: 'Dashboard',
+            href: '/dashboard',
+        },
+        {
+            label: 'Daftar siswa',
+            href: '/dashboard/admin/students',
+        },
+        {
+            label: 'Info siswa',
+        },
+    ];
+</script>
+
+<DashboardLayout {title} {breadcrumbItems}>
+    <div class="grid grid-cols-1">
+        <h2 class="text-xl font-bold mb-4">Data diri siswa</h2>
+
+        <a
+            href={route('dashboard.admin.students.index')}
+            class="mb-3 underline inline-flex items-center gap-1"
+            use:inertia
+        >
+            <ArrowLeftIcon class="size-[1.1em]" />
+            Kembali ke halaman utama siswa
+        </a>
+
+        <StudentInfolist {student} />
+    </div>
+</DashboardLayout>
